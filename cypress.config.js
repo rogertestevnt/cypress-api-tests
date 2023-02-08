d60fa95f3bd54622c970336c7b1b80dee937fe8d
rogertestevnt <rogertestevnt@gmail.com>
2023-02-08 08:38:07 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:"https://fakerestapi.azurewebsites.net/api/v1/"
+    baseUrl:"https://fakerestapi.azurewebsites.net/api/v1"
   },
   videoUploadOnPasses: false
   
